527356eac58b4d302b89ea613abbb024e2cd099b
VictorSoliman <vic@Victors-MacBook-Pro.local>
2016-04-13 07:48:57 +1000

    Functions 1-Create account. 2-Loggin 3-Check if logged in 4-Logout

--- controllers/HomeController.php
+++ controllers/HomeController.php
@@ -215,6 +215,84 @@
       {
          Session::destroy();
       }
+      
+      /*  author Victor
+       *    This function is for creating accounts. data will be saved into      *    database
+       */
+      public function register_account()
+      {
+  
+        $user_id = $_POST['value'];
+
+        $stmt = $this->db->prepare("INSERT INTO users (user_id) 
+        VALUES (:user_id)");
+        $stmt->bindParam(':user_id', $user_id);
+        $stmt->execute();
+        
+        echo "true";
+
+      }
+      /*  author Victor
+       *    This function is for loggin in , check id exists in database, if 
+       *    there is, get the favrourite data and
+       *    put them in session 
+       */
+
+      public function login()
+      {
+         $user_id = $_POST['value'];
+         $stmt = $this->db->prepare("SELECT user_id FROM users WHERE user_id = :user_id"); 
+         $stmt->bindParam(':user_id', $user_id);
+         $stmt->execute();
+         $total = $stmt->rowCount();
+
+
+          if($total>0)
+          {
+              Session::set("user_id",$user_id);
+
+             $stmt2 = $this->db->prepare("SELECT * FROM favourites WHERE user_id = :user_id"); 
+             $stmt2->bindParam(':user_id', $user_id);
+             $stmt2->execute();
+             $result = $stmt2->fetchAll();
+
+              $my_favourite = array();
+              $i = 0;
+             foreach($result as $row){
+
+                $new_favourite = array("city"=>$row['city'],"url"=>$row['url']);
+                array_push($my_favourite,$new_favourite); 
+
+              }
+
+              Session::set("my_favourite",$my_favourite);
+
+
+              echo "true";
+          }
+         else
+          {
+              echo "false";
+          } 
+
+      }      
+      /*  author Victor
+       *    check if logged in
+       */
+ 
+      public function loginChecked()
+      {
+          echo Session::get("user_id");
+      }
+      /*  author Victor
+       *    destory session 
+       */
+
+ 
+      public function logout()
+      {
+         Session::destroy();
+      }
 
       public function getEachStationJSON()
       {
